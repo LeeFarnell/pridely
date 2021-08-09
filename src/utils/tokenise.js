2de8dcab08cfb90bc960ce172b1e13dae1721468
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-08-09 18:47:00 +0100

    added code to verified token

--- src/utils/tokenise.js
+++ src/utils/tokenise.js
@@ -3,8 +3,14 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET || "mysupersecretsecret";
 const EXP_TIME = process.env.EXP_TIME || "2h";
 
+// created token with payload
 const tokenise = (payload) => {
   return jwt.sign(payload, JWT_SECRET, { expiresIn: EXP_TIME });
 };
 
-module.exports = tokenise;
+// we check the token for the secret and the age to make sure it is not expired
+const verifyToken = (token) => {
+  return jwt.verify(token, secret, { maxAge: expiresIn });
+};
+
+module.exports = { tokenise, verifyToken };
