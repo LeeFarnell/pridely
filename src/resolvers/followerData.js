62054a4ddf15065df412deb8f7662a046faddbe7
Tudor Tocan <tudor.tocan@gmail.com>
2021-08-15 17:38:52 +0100

    remove console.log from followerData resolver

--- src/resolvers/followerData.js
+++ src/resolvers/followerData.js
@@ -1,7 +1,6 @@
 const { User } = require("../models");
 
 const followerData = async (_, { followerId }) => {
-  console.log(followerId);
   return await User.findById(followerId);
 };
 module.exports = followerData;
