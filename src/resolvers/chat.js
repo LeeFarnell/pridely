01ff8b9fabb23b5a3cfbf5b08fb3a687bdf8fda9
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-08-19 21:16:29 +0100

    refactor messaging system

--- src/resolvers/chat.js
+++ src/resolvers/chat.js
@@ -1,16 +1,13 @@
 const { Message } = require("../models");
 
-const chat = async (_, { userId }) => {
-  if (userId) {
-    const messages = await Message.find({
-      $or: [{ fromUser: userId }, { toUser: userId }],
-    })
-      .populate("fromUser")
-      .populate("toUser");
-    return messages;
-  }
-
-  const messages = await Message.find({})
+const chat = async (_, { fromUserId, toUserId }) => {
+  // Brings back messaging history between 2 different user
+  const messages = await Message.find({
+    $or: [
+      { fromUser: fromUserId, toUser: toUserId },
+      { fromUser: toUserId, toUser: fromUserId },
+    ],
+  })
     .populate("fromUser")
     .populate("toUser");
   return messages;
--- src/schema/Subscription.js
+++
@@ -1,9 +0,0 @@
-const { gql } = require("apollo-server");
-
-const Subscription = gql`
-  type Subscription {
-    messageCreated(fromUser: ID!, toUser: ID!): Message
-  }
-`;
-
-module.exports = Subscription;
