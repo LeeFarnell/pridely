f7b1164c1e6d923b365d6d5cee85839be6b0bd31
Tudor Tocan <tudor.tocan@gmail.com>
2021-08-19 17:22:13 +0100

    install dependencies, bring in express server, wrap it in gql server, server runnning

--- src/server.js
+++ src/server.js
@@ -1,4 +1,9 @@
-const { ApolloServer } = require("apollo-server");
+const express = require("express");
+const { createServer } = require("http");
+const { execute, subscribe } = require("graphql");
+const { SubscriptionServer } = require("subscriptions-transport-ws");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { ApolloServer } = require("apollo-server-express");
 const mongoose = require("mongoose");
 
 const { DB_URL, MONGOOSE_OPTIONS } = require("./config/config");
@@ -9,15 +14,47 @@ const context = require("./context");
 mongoose.connect(DB_URL, MONGOOSE_OPTIONS);
 const db = mongoose.connection;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context,
-});
+(async function () {
+  const app = express();
 
-// this function will run the server
-db.once("open", () => {
-  server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+  const httpServer = createServer(app);
+
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
   });
-});
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  SubscriptionServer.create(
+    { schema, execute, subscribe },
+    { server: httpServer, path: server.graphqlPath }
+  );
+
+  const PORT = process.env.PORT || 4000;
+
+  db.once("open", () => {
+    httpServer.listen(PORT, () =>
+      console.log(`🚀  Server ready at  http://localhost:${PORT}/`)
+    );
+  });
+})();
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context,
+// });
+
+// // this function will run the server
+// db.once("open", () => {
+//   server.listen().then(({ url }) => {
+//     console.log(`🚀  Server ready at ${url}`);
+//   });
+// });
