856e1394d4efbbc2e78f71bfc0bd7d01607637fb
Tudor Tocan <tudor.tocan@gmail.com>
2021-08-27 22:28:43 +0100

    remove console.logs

--- src/resolvers/deletePost.js
+++ src/resolvers/deletePost.js
@@ -1,7 +1,6 @@
 const { Post } = require("../models");
 
 const deletePost = async (_, { postId }, context) => {
-  console.log(postId);
   return await Post.deleteOne({ _id: "6107fb24cb460159b45a721e" }).populate(
     "likes"
   );
--- src/resolvers/deleteUser.js
+++ src/resolvers/deleteUser.js
@@ -1,7 +1,6 @@
 const { User } = require("../models");
 
 const deleteUser = async (_, { userId }, context) => {
-  console.log(userId);
   return await User.deleteOne({ _id: "6107fb24cb460159b45a7214" });
 };
 
